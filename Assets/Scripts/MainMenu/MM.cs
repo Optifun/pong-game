19c9515bb9224c4b6acbf09e830e2daa0b3a09b8
DirtyBobby <dirtybobby12@gmail.com>
2019-11-15 22:36:44 +0700

    Мои изменения

--- Assets/Scripts/MainMenu/MM.cs
+++ Assets/Scripts/MainMenu/MM.cs
@@ -13,14 +13,67 @@ public class MM : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerSlider.onValueChanged.AddListener(SliderChanged);
+        BotSlider.onValueChanged.AddListener(BotSliderChanged);
+    }
 
+    public void StartGame()
+    {
+        Application.LoadLevel("Game");
+        PlayerSlider.minValue = 1;
+        SliderChanged(1);
+    }
+
+    public void BotSliderChanged(float val)
+    {
+        //BotSlider.value = val;
+        //if (PlayerSlider.value == 3)
+        //{
+        //    BotSlider.value = 1;
+        //}
+        //else if ((PlayerSlider.value == 1) && ((BotSlider.value != 1) || (BotSlider.value != 3)))
+        //{
+        //    BotSlider.value = 1;
+        //}
+        //else if ((PlayerSlider.value == 2) && (BotSlider.value != 2))
+        //{
+        //    BotSlider.value = 2;
+        //}
+        //else BotSlider.value = 0;
+
+
+
+        //Дописать
+        switch (PlayerSlider.value+val)
+        {
+            case 2:
+                {
+                    break;
+                }
+            case 3:
+                {
+                    break;
+                }
+            case 4:
+                {
+                    break;
+                }
+            default:
+                break;
+        }
+        BotInfoText.text = "Количество ботов " + BotSlider.value;
+    }
+
+    public void SliderChanged(float val)
+    {
+        PlayerSlider.value = val;
+        PlayerInfoText.text = "Количество игроков " + PlayerSlider.value;
+        BotSlider.value = val % 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        BotSlider.maxValue = 4 - PlayerSlider.value;
-        PlayerInfoText.text = "Количество игроков " + PlayerSlider.value;
-        BotInfoText.text = "Количество ботов " + BotSlider.value;
+
     }
 }
