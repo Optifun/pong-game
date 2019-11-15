e291c2f3501d027c6f22c5fb95a85482ac222707
DirtyBobby <dirtybobby12@gmail.com>
2019-11-15 18:18:41 +0700

    Menu

---
+++ Assets/Scripts/MainMenu/MM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MM : MonoBehaviour
+{
+
+    public Slider PlayerSlider;
+    public Text PlayerInfoText;
+    public Slider BotSlider;
+    public Text BotInfoText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BotSlider.maxValue = 4 - PlayerSlider.value;
+        PlayerInfoText.text = "Количество игроков " + PlayerSlider.value;
+        BotInfoText.text = "Количество ботов " + BotSlider.value;
+    }
+}
