01e6147cd78d2b582d50ab4330536a26f85d316f
DirtyBobby <dirtybobby12@gmail.com>
2020-01-27 20:13:50 +0700

    Ограничил скорость мяча
    Изменил скрипт BounceMovement

--- Assets/Custom/Scripts/BounceMovement.cs
+++ Assets/Custom/Scripts/BounceMovement.cs
@@ -6,6 +6,7 @@ public class BounceMovement : MonoBehaviour
 {
     Rigidbody rb;
     public GameObject Hit;
+    public float ballspeed = 7f;
     float timer;
     // Start is called before the first frame update
     void Start()
@@ -13,7 +14,6 @@ public class BounceMovement : MonoBehaviour
         timer = 0;
         rb = GetComponent<Rigidbody>();
         //сообщаем шару начальную скорость
-        var velocity = 0.35f;// Random.Range(0f, 1f);
         Vector3 way;
         int r = Random.Range(0, 4);
         float offset = 0;
@@ -26,16 +26,26 @@ public class BounceMovement : MonoBehaviour
         {
             offset = (r > 1) ? 0 : 1;
             var angle = Random.Range(-60f,60f)+offset*180;
-            Debug.Log(angle);
+            //Debug.Log(angle);
             way = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), 0, Mathf.Sin(Mathf.Deg2Rad * angle)).normalized;
         }
-        rb.AddForce(way * velocity);
+        rb.AddForce(way * ballspeed, ForceMode.VelocityChange);
         
     }
 
-    //
+    void FixedUpdate()
+    {
+       //Контроль скорости мяча
+        Debug.Log(rb.velocity.magnitude);
+        if (rb.velocity.magnitude != ballspeed)
+        {
+            rb.velocity = rb.velocity.normalized * ballspeed;
+        }
+    }
+    
     void Update()
     {
+        //Уничтожение шара по истечении времени
         timer += Time.deltaTime;
         if (timer >= 30)
             Destroy(gameObject);
@@ -49,7 +59,7 @@ public class BounceMovement : MonoBehaviour
 
             var hit = Instantiate(Hit,new Vector3(collision.contacts[0].point.x,
                                         collision.contacts[0].point.y,
-                                        collision.contacts[0].point.z),Quaternion.identity);    //спавн хита
+                                        collision.contacts[0].point.z),Quaternion.identity);    //спавн хита (эффект удара)
             Destroy(hit, 0.5f);
         }
     }
