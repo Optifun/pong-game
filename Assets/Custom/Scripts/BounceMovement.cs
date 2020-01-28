1e8b49d6ee4db05c61dc2fd3101e0e63e0b73cda
dirtybobby <vanya.frolov2001@gmail.com>
2020-01-28 14:44:30 +0700

    Контроль угла шара после столкновения
    - Изменил массу шара, т.к в прошлой версии шар выталкивал наши плиты
    - Теперь угол меняется не с помощью встроенной физики, а с помощью логики скрипта. Но это происходит некорректно, т.к надо ввести максимальный и минимальный угол и добавить в логику

--- Assets/Custom/Scripts/BounceMovement.cs
+++ Assets/Custom/Scripts/BounceMovement.cs
@@ -6,7 +6,8 @@ public class BounceMovement : MonoBehaviour
 {
     Rigidbody rb;
     public GameObject Hit;
-    public float ballspeed = 7f;
+    //Скорость шара
+    public float constBallSpeed = 7f;
     float timer;
     // Start is called before the first frame update
     void Start()
@@ -29,17 +30,17 @@ public class BounceMovement : MonoBehaviour
             //Debug.Log(angle);
             way = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), 0, Mathf.Sin(Mathf.Deg2Rad * angle)).normalized;
         }
-        rb.AddForce(way * ballspeed, ForceMode.VelocityChange);
+        rb.AddForce(way * constBallSpeed, ForceMode.VelocityChange);
         
     }
 
     void FixedUpdate()
     {
        //Контроль скорости мяча
-        Debug.Log(rb.velocity.magnitude);
-        if (rb.velocity.magnitude != ballspeed)
+        //Debug.Log(rb.velocity.magnitude);
+        if (rb.velocity.magnitude != constBallSpeed)
         {
-            rb.velocity = rb.velocity.normalized * ballspeed;
+            rb.velocity = rb.velocity.normalized * constBallSpeed;
         }
     }
     
@@ -55,6 +56,35 @@ public class BounceMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bar"))
         {
+            Rigidbody bar = collision.rigidbody;
+            Rigidbody ball = gameObject.GetComponent<Rigidbody>();
+
+            Vector3 forward = bar.transform.right;
+            Vector3 left = bar.GetComponent<PlayerBar>().Track.Left;
+            Vector3 barSpeed = Vector3.Dot(bar.velocity,left)*left;
+            Vector3 ballSpeed = ball.velocity;
+            ball.velocity += barSpeed.normalized * Vector3.Dot(forward, ballSpeed);
+            /*
+            // Угол альфа между нормалью и шаром, угол падения
+            float alphaAngle = Vector3.Angle(forward, ballSpeed);
+            // Вектор z
+            Vector3 deltaVectorReflection = barSpeed * Mathf.Sin(alphaAngle) * Vector3.Dot(barSpeed, ballSpeed) / ballSpeed.magnitude + ballSpeed;
+            
+            // Угол бета между нормалью и вектором z, угол отражения
+            float angleReflection = (deltaVectorReflection.magnitude>0) ? Vector3.Angle(deltaVectorReflection, forward) : alphaAngle;
+            // Дельта угла бета угол бета - угол альфа + он предотвращает ситуации, когда угол будет слишком тупой, т.е. > 160.
+            
+            //приводим угол к граничным условиям
+            angleReflection = (angleReflection < 0) ? 0 : angleReflection;
+            angleReflection = (angleReflection >= 80) ? 80 : angleReflection;
+
+            float deltaAngleReflection = angleReflection - alphaAngle;
+
+            float deltaBallSpeed = ballSpeed.magnitude * (Mathf.Cos(deltaAngleReflection) - Mathf.Cos(alphaAngle));
+
+            Vector3 localBallVelocity = (Vector3.Dot(ball.velocity, left)+ deltaBallSpeed) * left;
+            ball.velocity = localBallVelocity;
+            */
             gameObject.GetComponent<MeshRenderer>().material.color = collision.gameObject.GetComponent<MeshRenderer>().material.color;
 
             var hit = Instantiate(Hit,new Vector3(collision.contacts[0].point.x,
